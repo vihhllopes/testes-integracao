8d87cf356147aa44874c636aeba5c510a46d8b88
Maria Victoria <viictoria931@gmail.com>
2022-11-06 12:28:32 -0300

    TesteIntegração- Modulo 1

--- src/test/java/modulo1/integracao/IntegracaoTeste01.java
+++ src/test/java/modulo1/integracao/IntegracaoTeste01.java
@@ -10,6 +10,45 @@ import java.util.Arrays;
 import java.util.List;
 
 public class IntegracaoTeste01 {
+    @Test
+    public void getEstudantesByPeriodo() {
+        Estudante estudante = Mockito.mock(Estudante.class);
+        Mockito.when(estudante.findByPeriodo(2)).thenReturn(Arrays.asList("Maria Victória", "Katarina", "Arthur"));
+        List<String> estudantes = estudante.findByPeriodo(1);
+        Assertions.assertFalse(estudantes.size() > 2);
+    }
+
+    @Test
+    public void getEstudantesByPeriodoverifyTrue(){
+        Estudante estudante = Mockito.mock(Estudante.class);
+        Mockito.when(estudante.findByPeriodo(1)).thenReturn(Arrays.asList("Maria Victória", "Katarina", "Arthur"));
+        List<String> estudantes = estudante.findByPeriodo(1);
+        Assertions.assertTrue(estudantes.size()>2);
+        Mockito.verify(estudante).findByPeriodo(1);
+    }
+    @Test
+    public void getEstudantesByPeriodoverifyFalse(){
+        Estudante estudante = Mockito.mock(Estudante.class);
+        Mockito.when(estudante.findByPeriodo(2)).thenReturn(Arrays.asList("Maria Victória", "Katarina", "Arthur"));
+        List<String> estudantes = estudante.findByPeriodo(1);
+        Assertions.assertFalse(estudantes.size()>2);
+        Mockito.verify(estudante).findByPeriodo(1);
+    }
+    @Test
+    public void InsertEstudante(){
+        Estudante estudante = Mockito.mock(Estudante.class);
+        List<Estudante> estudantes = Arrays.asList(new Estudante());
+        Mockito.when(estudante.insertEstudantes(estudantes)).thenReturn(true);
+        Assertions.assertTrue(estudante.insertEstudantes(estudantes));
+    }
+    @Test
+    public void InsertEstudanteverify(){
+        Estudante estudante = Mockito.mock(Estudante.class);
+        List<Estudante> estudantes = Arrays.asList(new Estudante());
+        Mockito.when(estudante.insertEstudantes(estudantes)).thenReturn(true);
+        Assertions.assertTrue(estudante.insertEstudantes(estudantes));
+        Mockito.verify(estudante).insertEstudantes(estudantes);
+    }
 
 
 
