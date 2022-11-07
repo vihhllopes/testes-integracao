7803b1cd0e606cc011113389e46b4f763ae5d169
marcelle-mascarenhas <marcellebts@gmail.com>
2022-11-07 19:19:22 -0300

    feat: testes de integracao - modulo 3

--- src/test/java/modulo3/integracao/IntegracaoTest03.java
+++ src/test/java/modulo3/integracao/IntegracaoTest03.java
@@ -12,5 +12,42 @@ import java.util.List;
 public class IntegracaoTest03 {
 
 
+    @Test
+    public void addReceitaFacul(){
+        Faculdade faculdade = Mockito.mock(Faculdade.class);
+        Mockito.when(faculdade.getArrecadado()).thenReturn(100.0);
+
+        boolean pagamentoResult = Pagamento.transacao(faculdade, 100);
+        Mockito.verify(faculdade, Mockito.times(1)).addReceita(100);
+
+        Assertions.assertTrue(pagamentoResult);
+    }
+
+    @Test
+    public void getFaculdades(){
+        ListaFaculdade faculdades = Mockito.mock(ListaFaculdade.class);
+
+        Faculdade faculdade = new Faculdade();
+        faculdade.setNome("FAFIC");
+        faculdade.setArrecadado(2000000);
+
+        Mockito.when(faculdades.findAll()).thenReturn(Arrays.asList(faculdade));
+        List<Faculdade> faculdadeList = faculdades.findAll();
+
+        Assertions.assertEquals("FAFIC", faculdadeList.get(0).getNome());
+    }
+
+    @Test
+    public void getBibliotecas(){
+        ListaBiblioteca bibliotecas = Mockito.mock(ListaBiblioteca.class);
+
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.setNome("São Cristovão");
+
+        Mockito.when(bibliotecas.findAll()).thenReturn(Arrays.asList(biblioteca));
+        List<Biblioteca> bibliotecaList = bibliotecas.findAll();
+
+        Assertions.assertEquals("São Cristovão", bibliotecaList.get(0).getNome());
+    }
 
 }
